36df1305c850ce189fed25eff3a253d5410426d3
Yuer <18213857028@163.com>
2023-06-05 21:29:11 +0800

    delete wrong class

--- sift/main.cpp
+++ sift/main.cpp
@@ -1,11 +1,13 @@
 extern void test_match();
 extern void test_omp();
 extern void test_multithreading();
+extern void test_opencv_para();
 
 int main(int argc, char *argv[])
 {
 //    test_match();
 //    test_omp();
-    test_multithreading();
+//    test_multithreading();
+    test_opencv_para();
     return 0;
 }
